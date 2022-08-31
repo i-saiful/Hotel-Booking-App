64086982772de8cc3006ca3d945b3c99c047688d
Saiful-Lab <saifulislam_@outlook.com>
2022-08-31 11:03:17 +0600

    ADD Navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import Navigation from "./component/Navigation";
-
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div>
       <Navigation />
-      
+      <Home />
     </div>
   );
 }
--- src/component/Navigation.jsx
+++ src/component/Navigation.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 
 function Navigation() {
-  return (
-    <div className='navbar bd-light'>
-          <h1 className='nav-brand'>Hotel Booking App</h1>
-          <p className='nav-item'>Logout</p>
-    </div>
-  )
+    return (
+        <div className='navbar bg-light'>
+            <div className="container">
+                <h1 className='navbar-brand'>Hotel Booking App</h1>
+                <div className='d-flex align-items-center'>
+                    <button className='nav-item btn btn-primary'>Book now</button>
+                    <p className='nav-item m-0 ms-5'>Logout</p>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Navigation
