d4538fd231a4ca90ef17c2c1b0c47fbe87989745
Saiful-Lab <saifulislam_@outlook.com>
2022-08-31 10:42:29 +0600

    setup project

---
+++ src/component/Navigation.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Navigation() {
+  return (
+    <div className='navbar bd-light'>
+          <h1 className='nav-brand'>Hotel Booking App</h1>
+          <p className='nav-item'>Logout</p>
+    </div>
+  )
+}
+
+export default Navigation
